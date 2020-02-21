6a9672da8cfb071abf8b71504f86f82b2819aff8
CongAnh <conganh1991999@gmail.com>
2020-02-21 18:35:44 +0700

    home activity with bottom navigation

---
+++ app/src/main/java/com/camm/foodizz/ui/me/MeViewModel.java
@@ -0,0 +1,20 @@
+package com.camm.foodizz.ui.me;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class MeViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public MeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is me fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+
+}
